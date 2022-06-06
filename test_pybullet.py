54c25381f8bf4f45a21666fface4b5c3c4b2b44c
Chris Paxton <cpaxton@fb.com>
2022-06-06 10:19:16 -0500

    update stretch bullet setup + urdf

--- test_pybullet.py
+++ test_pybullet.py
@@ -1,5 +1,6 @@
 import pybullet as pb
 import pybullet_data
+import time
 
 client = pb.connect(pb.GUI)
 # pb.setAdditionalSearchPath('.')
@@ -13,3 +14,10 @@ start_orn = pb.getQuaternionFromEuler([0,0,0])
 
 # Load the stretch
 robot_id = pb.loadURDF('./urdf/stretch.urdf', start_pos, start_orn)
+
+for i in range (10000):
+    pb.stepSimulation()
+    time.sleep(1./240.)
+    pos, orn = pb.getBasePositionAndOrientation(robot_id)
+    print(pos, orn)
+pb.disconnect()

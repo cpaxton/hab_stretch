979808b2aa964d630c166e207e32530aaa06dc3f
Chris Paxton <cpaxton@fb.com>
2022-10-05 18:04:31 -0400

    add some tools to help understand your urdf

--- test_pybullet.py
+++ test_pybullet.py
@@ -1,6 +1,7 @@
 import pybullet as pb
 import pybullet_data
 import time
+from collections import namedtuple
 
 client = pb.connect(pb.GUI)
 # pb.setAdditionalSearchPath('.')
@@ -15,6 +16,19 @@ start_orn = pb.getQuaternionFromEuler([0, 0, -1.5607])
 # Load the stretch
 #robot_id = pb.loadURDF('./urdf/stretch.urdf', start_pos, start_orn)
 robot_id = pb.loadURDF('./urdf/hab_stretch_dex_wrist_simplified.urdf', start_pos, start_orn)
+
+# Get information about each joint
+PbJointInfo = namedtuple('JointInfo', ['index', 'name', 'type', 'qindex', 'uindex', 'flags',
+                                       'damping', 'friction', 'lower_limit', 'upper_limit',
+                                       'max_force', 'max_velocity', 'link_name', 'axis',
+                                       'parent_frame_pos', 'parent_frame_rot', 'parent_idx'])
+num_joints = pb.getNumJoints(robot_id, client)
+joint_infos = []
+for i in range(num_joints):
+    joint = PbJointInfo(*pb.getJointInfo(robot_id, i, client))
+    joint_infos.append(joint)
+
+    print(joint.index, joint.name, joint.link_name, joint.lower_limit, joint.upper_limit)
 
 for i in range (10000):
     pb.stepSimulation()
